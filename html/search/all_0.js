1627295c6e81a17a4463dcc5cf59bff84881d25e
Patrick TELLIER <tellierp@cyberdev.be>
2021-10-11 12:19:32 +0200

    add doxygen

---
+++ html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['monopoly_0',['monopoly',['../md__r_e_a_d_m_e.html',1,'']]]
+];
---
+++ html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['monopoly_1',['monopoly',['../md__r_e_a_d_m_e.html',1,'']]]
+];
